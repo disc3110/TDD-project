58f8ac81355e9989318246e6a1cbf61d9d97da1e
Diego Solis <disc3110@gmail.com>
2022-03-10 10:50:00 -0600

    make corrections to factorial test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,16 +1,13 @@
 require_relative '../solver'
 
 describe Solver do
-  before :each do
-    @solver = Solver.new
-  end
   it 'returns returns the correct factorial of n' do
-    expect(@solver.factorial(5)).to eq 120
+    expect(Solver.new.factorial(5)).to eq 120
   end
   it 'The factorial of 0 should return 1' do
-    expect(@solver.factorial(0)).to eq 1
+    expect(Solver.new.factorial(0)).to eq 1
   end
   it 'raises an error if a negative number is passed to a factorial' do
-    expect(@solver.factorial(-5)).to raise_exception
+    expect {Solver.new.factorial(-5)}.to raise_error(ArgumentError, 'number must not be negative')
   end
 end
