ac61af02095c7d8d389e70926dfac33494466a63
Diego Solis <disc3110@gmail.com>
2022-03-10 10:50:15 -0600

    write code for factorial

---
+++ solver.rb
@@ -0,0 +1,17 @@
+class Solver
+  def factorial(n)
+    return 1 if n == 0
+    raise ArgumentError, 'number must not be negative' if n.negative?
+
+    result = 1
+    until n < 1
+      result *= n
+      n -= 1
+    end
+    p(result)
+    result
+  end
+end
+
+solver = Solver.new
+p(solver.factorial(5))
