4078e09736d2fd59b5822ce3393ac24ea79e6e03
Anjacodes <anja_schmidt7@posteo.de>
2022-03-10 11:01:46 -0600

    Write test for reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,13 +1,26 @@
 require_relative '../solver'
 
 describe Solver do
-  it 'returns returns the correct factorial of n' do
-    expect(Solver.new.factorial(5)).to eq 120
+
+  before :each do
+    @solver = Solver.new
   end
-  it 'The factorial of 0 should return 1' do
-    expect(Solver.new.factorial(0)).to eq 1
+
+  describe '#factorial' do
+    it 'returns returns the correct factorial of n' do
+      expect(@solver.factorial(5)).to eq 120
+    end
+    it 'The factorial of 0 should return 1' do
+      expect(@solver.factorial(0)).to eq 1
+    end
+    it 'raises an error if a negative number is passed to a factorial' do
+      expect {@solver.factorial(-5)}.to raise_error(ArgumentError, 'number must not be negative')
+    end
   end
-  it 'raises an error if a negative number is passed to a factorial' do
-    expect {Solver.new.factorial(-5)}.to raise_error(ArgumentError, 'number must not be negative')
+
+  describe '#reverse' do
+    it 'takes a string as an argument and reverses its order' do
+      expect(@solver.reverse('hello')).to eql 'olleh'
+    end
   end
 end
